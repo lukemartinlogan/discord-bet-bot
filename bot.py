caeee1e6eb41551142115842a68f69cb9f8b0769
claxtastic <thomas.claxton.dev@gmail.com>
2021-11-20 23:40:53 -0600

    new stuff

---
+++ bot.py
@@ -0,0 +1,82 @@
+import os
+from gamble import Gamble
+from discord import Intents, Embed
+from discord.ext.commands import Bot
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option
+
+bot = Bot(command_prefix="1")
+slash = SlashCommand(bot)
+
+@slash.slash(name='register', description='Registers a user')
+async def register(ctx: SlashContext):
+    gamble = Gamble.GetInstance()
+    await ctx.send(gamble.register(f"<@!{ctx.author.id}>"))
+
+@slash.slash(
+    name='bet',
+    description='Bet credits on a user',
+    options=[
+        create_option(
+            name="on",
+            description="User to bet on",
+            option_type=3,
+            required=True
+        ),
+        create_option(
+            name="amt",
+            description="Number of credits to bet",
+            option_type=4,
+            required=True
+        )
+    ]
+)
+async def bet(ctx: SlashContext, on, amt):
+    gamble = Gamble.GetInstance()
+    better = f"<@!{ctx.author.id}>"
+    await ctx.send(gamble.bet(better, on, amt))
+
+@slash.slash(name='withdraw', description='Abstain from betting')
+async def withdraw(ctx: SlashContext):
+    gamble = Gamble.GetInstance()
+    await ctx.send(gamble.withdraw(f"<@!{ctx.author.id}>"))
+
+@slash.slash(
+    name='winner',
+    description='Input the winner of the round',
+    options=[
+        create_option(
+            name="winner",
+            description="The winner of the bet",
+            option_type=3,
+            required=True
+        ),
+        create_option(
+            name="squad_win",
+            description="Whether squad won or not",
+            option_type=5,
+            required=True
+        )
+    ]
+)
+async def winner(ctx: SlashContext, winner, squad_win):
+    gamble = Gamble.GetInstance()
+    await ctx.send(gamble.winner(winner, squad_win))
+
+@slash.slash(name='balance', description='Check user balance')
+async def balance(ctx: SlashContext):
+    gamble = Gamble.GetInstance()
+    embed = gamble.balance(f"<@!{ctx.author.id}>")
+    await ctx.send(embed=embed)
+
+@slash.slash(name='give', description='Give a user credits')
+async def give(ctx: SlashContext, user, amt):
+    gamble = Gamble.GetInstance()
+    await ctx.send(gamble.give(f"{user}", amt))
+
+@slash.slash(name='give_all', description='Give all users credits')
+async def give_all(ctx: SlashContext, amt):
+    gamble = Gamble.GetInstance()
+    await ctx.send(gamble.give_all(amt))
+
+bot.run(os.getenv('DISCORD_TOKEN'))
---
+++ gamble.py
@@ -0,0 +1,149 @@
+import os
+import json
+import numpy as np
+from discord import Embed
+
+class Gamble:
+    bot = None
+
+    @staticmethod
+    def GetInstance():
+        if Gamble.bot is None:
+            Gamble.bot = Gamble()
+        return Gamble.bot
+
+    def __init__(self):
+        self.is_loaded_ = False
+        self.users_ = {}
+        self.winning_squad_ = 4
+        self.winner_ = 2
+        self.load_results()
+
+    def load_results(self):
+        if not os.path.exists('bets.json'):
+            return
+        with open('bets.json', 'r') as fp:
+            info = json.load(fp)
+        self.users_ = info['users']
+        self.winning_squad_ = info['winning_squad']
+        self.winner_ = info['winner']
+        print(f"self.users_: {self.users_}")
+
+    def store_results(self):
+        info = {
+            'users': self.users_,
+            'winning_squad': self.winning_squad_,
+            'winner': self.winner_
+        }
+        with open('bets.json', 'w') as fp:
+            json.dump(info, fp, indent=4)
+
+    def register(self, name):
+        if name in self.users_:
+            return "You're already registered."
+        self.users_[name] = {
+            'balance': 25,
+            'bet-on': None,
+            'bet-amt': 0,
+            'borrow': 0
+        }
+        self.store_results()
+        return f"{name} is registered"
+
+    def bet(self, better, on, amt):
+        if better not in self.users_:
+            return f'{better} is not registered'
+        amt = float(amt)
+        if amt > self.users_[better]['balance']:
+            return f"Cannot bet more than your balance: {self.users_[better]['balance']}"
+        self.users_[better]['bet-on'] = on
+        self.users_[better]['bet-amt'] = amt
+
+        return f'{better} bets on {on} for {amt}'
+
+    def withdraw(self, id):
+        self.bet(id, None, 0)
+        return f"{id} abstains from betting"
+
+    def reset_bets(self):
+        for user in self.users_.keys():
+            self.withdraw(user)
+
+    def winner(self, winner, squad_win):
+        squad_win = True if squad_win == 'yes' else False
+
+        if not self.user_is_registered(winner):
+            return f'{winner} is not in the user list'
+
+        pot = sum([profile['bet-amt'] for profile in self.users_.values()])
+        correct_betters = [user for user,profile in self.users_.items() if profile['bet-on'] == winner]
+        correct_bets = np.array([(profile['bet-on'] == winner) for profile in self.users_.values()])
+        weights = np.array([profile['bet-amt'] for profile in self.users_.values()])
+        weights = weights*correct_bets
+        net_weight = np.sum(weights)
+        if net_weight:
+            weights /= net_weight
+        print(weights)
+
+        #Reward for guessing correctly
+        for profile,weight in zip(self.users_.values(), weights):
+            profile['gain'] = pot*weight - profile['bet-amt']
+            profile['balance'] += profile['gain']
+
+        #Reward for winning the game
+        if squad_win:
+            for profile in self.users_.values():
+                profile['gain'] += self.winning_squad_
+                profile['balance'] += self.winning_squad_
+
+        #Reward for doing most damage
+        self.users_[winner]['gain'] += self.winner_
+        self.users_[winner]['balance'] += self.winner_
+
+        #Withdraw all users at round end
+        self.reset_bets()
+
+        #Print winners and scores
+        if net_weight == 0:
+            congrats = 'None of you predicted correctly. Losers.\n'
+        else:
+            congrats = f"Congrats! The following sweaty people guessed correctly: {','.join(correct_betters)}\n"
+        scores = ""
+        for user,profile in self.users_.items():
+            if profile['gain'] >= 0:
+                scores += f"{user}: gain={profile['gain']}, balance={profile['balance']} shmeckles\n"
+            else:
+                scores += f"{user}: loss={-profile['gain']}, balance={profile['balance']} shmeckles\n"
+        return congrats + scores
+
+    def balance(self, id):
+        if not self.user_is_registered(id):
+            return f'{id} is not in the user list'
+        embed = self.balance_embed(id, self.users_[id])
+        return embed
+
+    def give(self, user, amt):
+        if not self.user_is_registered(user):
+            return f'{user} is not a valid user'
+        amt = float(amt)
+        self.users_[user]['balance'] += amt
+        return f"balance={self.users_[user]['balance']} shmeckles"
+
+    def give_all(self, amt):
+        for profile in self.users_.values():
+            profile['balance'] += float(amt)
+        return json.dumps(self.users_, indent=4)
+
+    def user_is_registered(self, id):
+        if id not in self.users_:
+            return False
+        else:
+            return True
+
+    def balance_embed(self, user, balance_dict):
+        embed = Embed(title=f"{user}'s balance")
+        embed.add_field(name='Balance', value=balance_dict['balance'], inline=True)
+        embed.add_field(name='Bet on', value=balance_dict['bet-on'], inline=True)
+        embed.add_field(name='Borrow', value=balance_dict['borrow'], inline=True)
+        embed.add_field(name='Gain', value=balance_dict['borrow'], inline=True)
+        return embed
